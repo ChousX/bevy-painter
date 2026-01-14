17008be96d2fc16f5b5a92bb750e13ff7c234965
ChousX <gmanenergy@gmail.com>
2026-01-14 15:07:24 -0500

    fixed a example

--- examples/visibility_terrain.rs
+++ examples/visibility_terrain.rs
@@ -132,7 +132,7 @@ fn setup(
         Transform::from_xyz(4.0, 8.0, 4.0).looking_at(Vec3::ZERO, Vec3::Y),
     ));
 
-    commands.insert_resource(AmbientLight {
+    commands.spawn(AmbientLight {
         color: Color::WHITE,
         brightness: 300.0,
         ..default()
